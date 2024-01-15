a2648e03bcac31db201e8096226c52bf943a6310
Kamal Soltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2024-01-15 14:41:22 +0400

    Add files via upload

---
+++ music/index.js
@@ -0,0 +1,44 @@
+let songs = [
+    {
+        name: 'West Coast',
+        path: 'songs/WestCoast.mp3',
+        artist: 'Lana Del Rey',
+        cover: 'img/lana.jpg'
+    },
+    {
+        name: 'I Wanna Be Yours',
+        path: 'songs/IWBY.mp3',
+        artist: 'Arctic Monkeys',
+        cover: 'img/ArcticMonkeys.jpg'
+    },
+    {
+        name: 'Sweater Weather',
+        path: 'songs/SweaterWeather.mp3',
+        artist: 'The Neighbourhood',
+        cover: 'img/NBD.jpg'
+    },
+    {
+        name: 'Jailhouse Rock',
+        path: 'songs/JailhouseRock.mp3',
+        artist: 'Elvis Presley',
+        cover: 'img/elvis.jpg'
+    },
+    {
+        name: 'Smells Like Teen Spirit',
+        path: 'songs/slts.mp3',
+        artist: 'Nirvana',
+        cover: 'img/nirvana.jpeg'
+    },
+    {
+        name: 'Take Me To Church',
+        path: 'songs/tmtc.mp3',
+        artist: 'Hozier',
+        cover: 'img/hozier.jpeg'
+    },
+    {
+        name: 'Stuck In The Middle With You',
+        path: 'songs/sitmwy.mp3',
+        artist: 'Stealers Wheel',
+        cover: 'img/sw.jpeg'
+    },
+]
